1f4848344c61a89884929e3dd66f75b20d1ec4f6
Gurdeep Singh <g.s.basantpuria@gmail.com>
2024-11-02 13:49:20 -0400

    Add more code

--- Todo with contextApi/src/components/TodoItem.jsx
+++ Todo with contextApi/src/components/TodoItem.jsx
@@ -3,10 +3,21 @@ import React from "react";
 import { useTodo } from "../contexts/TodoContext";
 
 function TodoItem({ todo }) {
-  const { isTodoEditable, setIsTodoEditable } = useState(false);
+  const [isTodoEditable, setIsTodoEditable] = useState(false);
 
   const [todoMsg, setTodoMsg] = useState(todo.todo);
-  const { updateTodo, deleteTodo, toggleCompleted } = useTodo();
+
+  const { updateTodo, deleteTodo, toggleComplete } = useTodo();
+
+  const editTodo = () => {
+    updateTodo(todo.id, { ...todo, todo: todoMsg });
+    setIsTodoEditable(false);
+  };
+
+  const toggleCompleted = () => {
+    toggleComplete(todo.id);
+  };
+
   return (
     <div
       className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
