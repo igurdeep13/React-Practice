a7ddd2b6a90e5d013a3d260fcd32fa597647df33
Gurdeep Singh <g.s.basantpuria@gmail.com>
2024-11-26 01:39:58 -0500

    add new code

--- 01MegaBlog/src/components/Select.jsx
+++ 01MegaBlog/src/components/Select.jsx
@@ -1,22 +1,23 @@
-import React, { useId } from "react";
+import { useId } from "react";
+import React from "react";
 
 function Select({ options, label, className = "", ...props }, ref) {
   const id = useId();
   return (
     <div className="w-full">
       {label && <label htmlFor={id} className=""></label>}
-      <Select
+      <select
         {...props}
         id={id}
         ref={ref}
-        className={`px-3 py-2 rounded-lg bg-white text-black ouline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
       >
         {options?.map((option) => (
-          <options key={option} value={option}>
+          <option id={option} value={option}>
             {option}
-          </options>
+          </option>
         ))}
-      </Select>
+      </select>
     </div>
   );
 }
