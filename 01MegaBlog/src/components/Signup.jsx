7ff21fd253c1eb06e600e778fe1d510fd29d04eb
Gurdeep Singh <g.s.basantpuria@gmail.com>
2024-11-27 01:26:25 -0500

    add new project

--- 01MegaBlog/src/components/Signup.jsx
+++ 01MegaBlog/src/components/Signup.jsx
@@ -7,6 +7,23 @@ import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
 
 function Signup() {
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
+
+  const create = async (data) => {
+    setError("");
+    try {
+      const userData = await authService.createAccount(data);
+      if (userData) {
+        const userData = await authService.getCurrentUser();
+        if (userData) dispatch(login(userData));
+        navigate("/");
+      }
+    } catch (error) {}
+  };
+
   return <div>Signup</div>;
 }
 
